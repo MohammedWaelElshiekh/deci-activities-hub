ab9ae5027955098b168a5bdc95b901a511a66cad
Mohammed Wael Elshiekh <mohammedwaelelshiekhmw@gmail.com>
2024-04-20 09:58:16 +0200

    made the fetch data work

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // setup the ability to see into response bodies
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // setup the express assets path
-app.use('/', express.static(path.join(__dirname, '../client')))
+app.use("/", express.static(path.join(__dirname, "../client")));
 
 // API calls ------------------------------------------------------------------------------------
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, './client/pages/user.html'));
-})
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/pages/user.html"));
+});
+const User = require("./user");
+app.get("/users", async (req, res) => {
+  const data = await fetch("https://dummyjson.com/users"); // array of user
+  data.forEach((user) => {
+    const newUser = new User(user.id, user.firstName, user.lastName);
+    newUser.displayInfo();
+  });
+});
 
-app.get('/users', async (req, res) => {
-    // Write your logic here
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
